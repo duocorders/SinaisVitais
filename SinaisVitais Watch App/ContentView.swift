b9fcf9ec099d8a1dee575439c2809022c375dc26
Pedro Godri <86849487+pedrogodri@users.noreply.github.com>
2026-05-29 10:14:54 -0300

    Initial Commit

---
+++ SinaisVitais Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SinaisVitais Watch App
+//
+//  Created by Pedro Henrique Godri on 29/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SinaisVitais Watch App/SinaisVitaisApp.swift
@@ -0,0 +1,17 @@
+//
+//  SinaisVitaisApp.swift
+//  SinaisVitais Watch App
+//
+//  Created by Pedro Henrique Godri on 29/05/26.
+//
+
+import SwiftUI
+
+@main
+struct SinaisVitais_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
